d9a080405488d493b1f29ab75f99a4204ded6cdd
obaadz <obada347@gmail.com>
2024-05-19 19:35:55 +0300

    Update

--- src/services/book.service.ts
+++ src/services/book.service.ts
@@ -1,9 +1,10 @@
+import { IsNull } from 'typeorm';
 import { Book } from '../entities/book.entity.js';
 
 export class BookService {
   public static async getAll(query: { library: number | null }): Promise<Book[]> {
     if (!query.library)
-      return Book.find({ where: { library: null } });
+      return Book.find({ where: { library: IsNull() } });
 
     return Book.find({ where: { library: { id: query.library } } });
   }
