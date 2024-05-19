945822a9ddb59b220c87d04d65aa513a85f3d5c1
obaadz <obada347@gmail.com>
2024-05-19 19:32:03 +0300

    Update

--- src/routes/library.route.ts
+++ src/routes/library.route.ts
@@ -4,5 +4,6 @@ import { LibraryController } from "../controllers/library.controller.js";
 const libraryRouter = Router();
 
 libraryRouter.get('/api/libraries', LibraryController.getAll);
+libraryRouter.get('/api/libraries/:libraryId/books', LibraryController.getAllLibraryBooks);
 
 export { libraryRouter }
