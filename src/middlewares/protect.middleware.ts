65af7fa7fb7d1537c073a9ec3bc04239dcea621e
obaadz <obada347@gmail.com>
2024-05-20 03:28:04 +0300

    Update

--- src/middlewares/protect.middleware.ts
+++ src/middlewares/protect.middleware.ts
@@ -3,19 +3,29 @@ import { UserService } from '../services/user.service.js'
 
 export class ProtectMiddleware {
   public static protect(req: Request, res: Response, next: NextFunction): void | Promise<void> {
-    const authorizationHeader = req.headers["authorization"],
-      token = authorizationHeader?.split(' ')[1]
+    try {
 
-    if (!token) {
-      res.status(401).send({ data: null, error: { message: 'Unauthorized' } })
-      return
+      const authorizationHeader = req.headers["authorization"],
+        token = authorizationHeader?.split(' ')[1]
+
+      if (!token) {
+        res.status(401).send({ data: null, error: { message: 'Unauthorized' } })
+        return
+      }
+
+      const data = UserService.verifyToken(token)
+
+      if (!data) {
+        res.status(401).send({ data: null, error: { message: 'Unauthorized' } })
+        return
+      }
+
+      req.auth = { user: { id: data.id }, token }
+
+      next()
+    } catch (error) {
+      console.error(error);
+      res.status(500).send({ data: null, error: { message: 'Internal server error' } });
     }
-
-    const data = UserService.verifyToken(token)
-
-    req.auth = { user: { id: data.id }, token }
-
-    next()
   }
-
 }
--- src/routes/user.route.ts
+++ src/routes/user.route.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import { UserController } from "../controllers/user.controller.js";
+import { ProtectMiddleware } from "../middlewares/protect.middleware.js";
 
 const userRouter = Router();
 
-userRouter.get('/api/me', UserController.getMe);
+userRouter.get('/api/me', ProtectMiddleware.protect, UserController.getMe);
 
-userRouter.get('/api/me/cart', UserController.getMeCart);
+userRouter.get('/api/me/cart', ProtectMiddleware.protect, UserController.getMeCart);
 
 export { userRouter }
