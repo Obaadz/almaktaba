c806f32f8d58351f48326a8b25253516e3ac5996
obaadz <obada347@gmail.com>
2024-05-21 01:40:00 +0300

    Update

--- src/admin/resources/cart-item.resource.ts
+++ src/admin/resources/cart-item.resource.ts
@@ -3,6 +3,7 @@ import { CartItem } from "../../entities/cart-item.entity.js";
 
 const cartItemResource: AdminJSOptions['resources'][number] = {
   resource: CartItem,
+  options: { navigation: false },
 }
 
 export default cartItemResource;
--- src/admin/resources/cart.resource.ts
+++ src/admin/resources/cart.resource.ts
@@ -3,6 +3,7 @@ import { Cart } from "../../entities/cart.entity.js";
 
 const cartResource: AdminJSOptions['resources'][number] = {
   resource: Cart,
+  options: { navigation: false },
 }
 
 export default cartResource;
