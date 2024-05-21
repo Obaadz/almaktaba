6d5fefc8ace79c55f7af2e13a22e1e72e46b0a59
obaadz <obada347@gmail.com>
2024-05-21 05:11:08 +0300

    Update

--- src/admin/actions/before-edit-order.ts
+++ src/admin/actions/before-edit-order.ts
@@ -1,6 +1,10 @@
-export const beforeEditOrder = (req, ctx) => {
+import { Order } from "../../entities/order.entity.js"
+
+export const beforeEditOrder = async (req, ctx) => {
   if (req.method === "post") {
     req.payload.cartItems = '' // Fix validation error when editing order
+
+    await Order.update({ id: req.params.id }, { code: req.payload.code, note: req.payload.note })
   }
 
   return req
