fd0fd4f1873919b0f00b44b8efc9395735a947b3
obaadz <obada347@gmail.com>
2024-05-22 17:03:12 +0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -91,3 +91,12 @@ const start = async () => {
 };
 
 start();
+
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+})
+
+process.on('uncaughtException', (error) => {
+  console.error('Uncaught Exception thrown', error);
+})
