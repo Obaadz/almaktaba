bdc94d476e373ded3977ce0a678f5df07f75fd3e
obaadz <obada347@gmail.com>
2024-05-25 03:07:18 +0300

    Update

--- src/admin/actions/after-edit-room.ts
+++ src/admin/actions/after-edit-room.ts
@@ -1,11 +1,13 @@
+import { UserService } from "../../services/user.service.js"
 import { RoomService } from "../../services/room.service.js"
 
 export const afterEditRoom = async (res, req, ctx) => {
   if (req.method === "post") {
     const room = await RoomService.getRoomById(req.payload.id)
-    console.log(req.payload)
+    const user = await UserService.getUserById(req.payload['owner.id'])
+
     room.name = req.payload.name
-    room.owner = req.payload.owner
+    room.owner = user
 
     await room.save()
   }
