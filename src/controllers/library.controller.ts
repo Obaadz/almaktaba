4f4270f1fbaa419a3248013939bafbb12cd55245
obaadz <obada347@gmail.com>
2024-06-02 04:52:27 +0300

    Update

--- src/controllers/library.controller.ts
+++ src/controllers/library.controller.ts
@@ -1,13 +1,25 @@
 import type { Request, Response } from 'express';
 import { LibraryService } from '../services/library.service.js';
 import { BookService } from '../services/book.service.js';
+import { getDistanceFromLatLonInMeters } from '../utils/get-distance-from-lat-lon-in-meters.js';
 
 export class LibraryController {
   public static async getAll(req: Request, res: Response): Promise<void> {
     try {
       const libraries = await LibraryService.getAll()
 
-      res.status(200).send({ data: { libraries }, error: null });
+      const sortedLibraries = req.query.lat && req.query.lng ? libraries.map(library => {
+        const distance = getDistanceFromLatLonInMeters(
+          Number(req.query.lat),
+          Number(req.query.lng),
+          Number(library.lat),
+          Number(library.lng)
+        )
+
+        return { ...library, distance }
+      }).sort((a, b) => a.distance - b.distance) : libraries
+
+      res.status(200).send({ data: { libraries: sortedLibraries }, error: null });
     } catch (error) {
       console.error(error);
       res.status(500).send({ data: null, error: { message: 'Internal server error' } });
