6d65f021248cc33bdc9b680f35910ba723ad3005
obaadz <obada347@gmail.com>
2024-06-04 07:26:41 +0300

    Update

--- src/app.ts
+++ src/app.ts
@@ -70,8 +70,8 @@ const start = async () => {
 
   app.use(admin.options.rootPath, router);
 
-  app.use(express.json())
-  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json({ limit: '50mb' }))
+  app.use(express.urlencoded({ extended: true, limit: '50mb' }))
 
   app.use(morgan('dev'))
 
